57323d7f94f4ed2ec0c95d679a053988a0964a35
devedbox <devedbox@qq.com>
2018-05-07 11:56:13 +0800

    Updated the content lints to checking the trailing of the triggering content' line.

--- Sources/PathLintFramework/Types/FileContentsRule.swift
+++ Sources/PathLintFramework/Types/FileContentsRule.swift
@@ -35,24 +35,31 @@ extension FileContentsRule {
                                  encoding: .utf8)
         return try NSRegularExpression(pattern: pattern, options: [.caseInsensitive])
             .matches(in: content, options: [.reportCompletion], range: NSRange(location: 0, length: (content as NSString).length))
-            .optionalMap {
-                if  let fixing = self.fixing {
-                    
-                    let pattern = "\(self.pattern)\\s//\\s\(NSRegularExpression.escapedPattern(for: fixing))"
-                    let range = NSRange(location: $0.range.location,
-                                        length: min((content as NSString).length - $0.range.location, $0.range.length
-                                            + (fixing as NSString).length
-                                            + 4))
-                    guard
-                        try NSRegularExpression(pattern: pattern, options: [.caseInsensitive])
-                            .matches(in: content, options: [.reportCompletion], range: range)
-                            .isEmpty
-                        else {
+            .optionalMap { result in
+                if let fixing = self.fixing {
+                    // Find the line where the result lay on.
+                    var totalLength = 0
+                    let lines = content.components(separatedBy: .newlines).map { comp in
+                        let range = NSRange(location: totalLength, length: (comp as NSString).length)
+                        totalLength += range.length + 1
+                        return range
+                    }.filter { range in
+                        return (result.range.location >= range.location
+                            && (result.range.location + result.range.length) <= (range.location + range.length))
+                    }.map { range in
+                        (content as NSString).substring(with: range)
+                    }
+                    // Checking the fixing format.
+                    if lines.count == 1 {
+                        let line = lines.last!
+                        // The fixing format should be the end of the range.
+                        if NSPredicate(format: "SELF ENDSWITH[cd] '\(fixing)'").evaluate(with: line) {
                             return nil
+                        }
                     }
                 }
                 
-                let range = content.range(from: NSRange(location: 0, length: $0.range.location))
+                let range = content.range(from: NSRange(location: 0, length: result.range.location))
                 let prefix = content[range]
                 let lines = prefix.components(separatedBy: .newlines)
                 let violation = Violation(line: lines.count,
