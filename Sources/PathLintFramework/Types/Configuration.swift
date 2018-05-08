e051e637be53caef5e947dc4796b3e5d2563e316
devedbox <devedbox@qq.com>
2018-05-08 10:21:16 +0800

    Added annotations comments.

--- Sources/PathLintFramework/Types/Configuration.swift
+++ Sources/PathLintFramework/Types/Configuration.swift
@@ -8,30 +8,54 @@
 import Foundation
 import Yams
 
+// MARK: - ConfigurationError.
+
+/// Error type represents the error info of configuration domain.
 public enum ConfigurationError: String, Error {
+    /// Indicates the file does not exist at the given path.
     case fileNotExists = "There is no configuration file exists."
+    /// Indicates the data of the file is invalid.
     case invalidData = "Invalid configuration data."
 }
 
+// MARK: - Configuration.
+
+/// A type represents the only configuration of the whole lint target, a configuration consists of
+/// the basic path pattern, global contents rule, rules to lint paths and excludes path to be excluded.
+///
+/// The configuration contains rules and excludes, and basic pattern. Using the basic pattern can
+/// optimize the size of the configuration file.
 public struct Configuration: Decodable {
-    enum CodingKeys: String, CodingKey {
+    /// Coding keys of `Configuration`.
+    internal enum CodingKeys: String, CodingKey {
+        /// Coding key for `base_pattern`.
         case basePattern = "base_pattern"
+        /// Coding key for `global_content_rules`.
         case globalContentRules = "global_content_rules"
+        /// Coding key for `rules`.
         case rules
+        /// Coding key for `excludes`.
         case excludes
     }
-    
+    /// Basic pattern to be used commonality.
     let basePattern: String
+    /// Global content rules to verify each file of the given path.
     let globalContentRules: [FileContentsRule]?
+    /// Rules of path lints.
     let rules: [PathRule]
+    /// Excluding paths.
     let excludes: [String] // The file directory to be excluded.
 }
+
+// MARK: -
 
 extension Configuration {
     public static func `default`() throws -> Configuration {
         return try config(at: getcwd())
     }
 }
+
+// MARK: -
 
 extension Configuration {
     public static func config(at path: String) throws -> Configuration {
