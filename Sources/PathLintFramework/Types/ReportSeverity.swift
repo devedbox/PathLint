421b4c62fd418051305e7a4b991a3fc009189c84
devedbox <devedbox@qq.com>
2018-05-08 10:11:30 +0800

    Added mark annotation comments.

--- Sources/PathLintFramework/Types/ReportSeverity.swift
+++ Sources/PathLintFramework/Types/ReportSeverity.swift
@@ -5,6 +5,8 @@
 //  Created by devedbox on 2018/4/9.
 //
 
+// MARK: - ReportSeverity.
+
 /// A type represents the report type of the lint result.
 public enum ReportSeverity: String, Decodable {
     /// Value indicates the warnings of the lint result.
@@ -12,3 +14,4 @@ public enum ReportSeverity: String, Decodable {
     /// Value indicates the errors of the lint result.
     case error
 }
+
