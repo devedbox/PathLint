9d30a1243a718fdb211328b5aca2865a507b8f45
devedbox <devedbox@qq.com>
2018-05-08 10:35:04 +0800

    Added Spec.swift of PathLintFramework.

---
+++ Sources/PathLintFramework/Types/Spec.swift
@@ -0,0 +1,22 @@
+//
+//  Spec.swift
+//  PathLintFramework
+//
+//  Created by devedbox on 2018/5/8.
+//
+
+// MARK: - Spec.
+
+/// A type represents the info of `PathLintFramework`.
+public struct Spec { }
+/// The global instance of `PathLintFramework`.
+public let spec = Spec()
+
+// MARK: -
+
+extension Spec {
+    /// Returns the current version of `PathLintFramework`.
+    public var version: String {
+        return "0.0.7"
+    }
+}
